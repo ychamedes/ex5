c417b646f92a07b53178ddfcb57aade26c8fb4c5
jasgreenspan <34744366+jasgreenspan@users.noreply.github.com>
2018-06-03 17:59:51 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,6 +1,15 @@
 import java.io.File;
 import java.util.HashSet;
 
+/**
+ * Order interface for order classes (each sorts its own way).
+ */
 public interface Order {
+
+    /**
+     * Sorts a set of files into an ordered array.
+     * @param files the set of files to be sorted.
+     * @return the sorted array of files.
+     */
     File[] sort(HashSet<File> files);
 }
