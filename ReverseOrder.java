a1ce360b2f97f43178a9bae1e1bbd0fbd0622968
jasgreenspan <34744366+jasgreenspan@users.noreply.github.com>
2018-06-03 18:01:15 +0300

    Update ReverseOrder.java

--- ReverseOrder.java
+++ ReverseOrder.java
@@ -3,10 +3,17 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
+/**
+ * ReverseOrder class that functions as a decorator on a RegularOrder class, making them reverse their order.
+ */
 public class ReverseOrder implements Order {
 
     private RegularOrder order;
 
+    /**
+     * Constructor for a ReverseOrder instance.
+     * @param order - the RegularOrder instance that is being reversed
+     */
     ReverseOrder(RegularOrder order){
         this.order = order;
     }
