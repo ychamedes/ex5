a90ddae363928339edd20fe2bca1fdc26af9c7b2
jasgreenspan <34744366+jasgreenspan@users.noreply.github.com>
2018-06-05 12:23:36 +0300

    Update FilterFactory.java

--- FilterFactory.java
+++ FilterFactory.java
@@ -1,31 +1,48 @@
 public class FilterFactory {
 
-    private static final String GREATER_COMMAND = "greater_than";
-    private static final String BETWEEN_COMMAND = "between";
-    private static final String SMALLER_COMMAND = "smaller_than";
+    protected static final String GREATER_COMMAND = "greater_than";
+    protected static final String BETWEEN_COMMAND = "between";
+    protected static final String SMALLER_COMMAND = "smaller_than";
     private static final String FILENAME_COMMAND = "file";
     private static final String CONTAINS_COMMAND = "contains";
     private static final String PREFIX_COMMAND = "prefix";
-    private static final String SUFFIX_COMMAND = "sufffix";
-    private static final String WRITABLE_COMMAND = "writable";
-    private static final String EXECUTABLE_COMMAND = "executable";
-    private static final String HIDDEN_COMMAND = "hidden";
+    private static final String SUFFIX_COMMAND = "suffix";
+    protected static final String WRITABLE_COMMAND = "writable";
+    protected static final String EXECUTABLE_COMMAND = "executable";
+    protected static final String HIDDEN_COMMAND = "hidden";
     private static final String ALL_COMMAND = "all";
+    protected static final String[] VALID_FILTERS = {GREATER_COMMAND, BETWEEN_COMMAND, SMALLER_COMMAND, FILENAME_COMMAND,
+        CONTAINS_COMMAND, PREFIX_COMMAND, SUFFIX_COMMAND, WRITABLE_COMMAND, EXECUTABLE_COMMAND, HIDDEN_COMMAND,
+        ALL_COMMAND};
 
 
     public static Filter buildFilter(String filterType, FilterParameter...parameters){
-        if(filterType.equals(GREATER_COMMAND)) return new GreaterThanFilter(parameters);
-        else if(filterType.equals(BETWEEN_COMMAND)) return new BetweenFilter(parameters);
-        else if(filterType.equals(SMALLER_COMMAND)) return new SmallerThanFilter(parameters);
-        else if(filterType.equals(FILENAME_COMMAND)) return new FileNameFilter(parameters);
-        else if(filterType.equals(CONTAINS_COMMAND)) return new NameContainsFilter(parameters);
-        else if(filterType.equals(PREFIX_COMMAND)) return new PrefixFilter(parameters);
-        else if(filterType.equals(SUFFIX_COMMAND)) return new SuffixFilter(parameters);
-        else if(filterType.equals(WRITABLE_COMMAND)) return new WritableFilter();
-        else if(filterType.equals(EXECUTABLE_COMMAND)) return new ExecutableFilter();
-        else if(filterType.equals(HIDDEN_COMMAND)) return new HiddenFilter();
-        //if(filterType.equals(ALL_COMMAND)) return new AllFilter();
-        else return new HiddenFilter();
+        switch (filterType) {
+            case GREATER_COMMAND:
+                return new GreaterThanFilter(parameters);
+            case BETWEEN_COMMAND:
+                return new BetweenFilter(parameters);
+            case SMALLER_COMMAND:
+                return new SmallerThanFilter(parameters);
+            case FILENAME_COMMAND:
+                return new FileNameFilter(parameters);
+            case CONTAINS_COMMAND:
+                return new NameContainsFilter(parameters);
+            case PREFIX_COMMAND:
+                return new PrefixFilter(parameters);
+            case SUFFIX_COMMAND:
+                return new SuffixFilter(parameters);
+            case WRITABLE_COMMAND:
+                return new WritableFilter();
+            case EXECUTABLE_COMMAND:
+                return new ExecutableFilter();
+            case HIDDEN_COMMAND:
+                return new HiddenFilter();
+//            case ALL_COMMAND:
+//                return new AllFilter();
+            default:
+                return new HiddenFilter();
+        }
         
     }
 }
