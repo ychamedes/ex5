65abc9802b07e44ebedabea5bb58495f46f60625
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-06 18:09:33 +0300

    Update FilterParameter.java

--- FilterParameter.java
+++ FilterParameter.java
@@ -1,18 +1,18 @@
 public class FilterParameter {
 
-    private int intParam;
+    private double doubleParam;
     private String stringParam;
 
-    FilterParameter(int intVar){
-        intParam = intVar;
+    FilterParameter(double doubleVar){
+        doubleParam = doubleVar;
     }
 
     FilterParameter(String stringVar){
         stringParam = stringVar;
     }
 
-    int getIntParam() {
-        return intParam;
+    double getDoubleParam() {
+        return doubleParam;
     }
 
     String getStringParam(){
