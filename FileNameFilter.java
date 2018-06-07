5ec0f2f1fecb7e1fd635397c760593d64b00dc78
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:57:56 +0300

    Update FileNameFilter.java

--- FileNameFilter.java
+++ FileNameFilter.java
@@ -1,7 +1,14 @@
 package filesprocessing;
 
+/**
+ * FileNameFilter class that selects for files that match a given name.
+ */
 public class FileNameFilter extends NameFilter {
 
+    /**
+     * FileNameFilter constructor thar receives a string in the parameters.
+     * @param parameters the string parameter to compare.
+     */
     FileNameFilter(String[] parameters){
         super(parameters);
     }
