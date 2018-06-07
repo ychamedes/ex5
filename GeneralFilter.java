7ce0d3ae9286027e95e773d978f247e8624b7efd
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:58:40 +0300

    Update GeneralFilter.java

--- GeneralFilter.java
+++ GeneralFilter.java
@@ -4,11 +4,29 @@ import java.io.File;
 import java.util.HashSet;
 
 /**
- *
+ * GeneralFilter class, an abstract filter class that is inherited by filters that compare attributes, or
+ * other general purpose filters that may be added to the module.
  */
+
 public abstract class GeneralFilter implements Filter{
 
-    /** */
+    protected static final String YES_PARAMETER = "YES";
+    protected static final String NO_PARAMETER = "NO";
+
+    /** A field to store a parameter for the filter to compare to. */
+    protected String generalParam;
+
+    //public GeneralFilter(){}
+
+    public GeneralFilter(String generalParameter){
+        this.generalParam = generalParameter
+    }
+
+    /**
+     * Judges the file based on some attribute.
+     * @param file the file to be analyzed
+     * @return true if the file name meets the condition.
+     */
     protected abstract boolean attribute(java.io.File file);
 
     @Override
