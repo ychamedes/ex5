93a113fc8a3bbac5ffe1fee04556ae5451c38f88
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:59:33 +0300

    Update NameContainsFilter.java

--- NameContainsFilter.java
+++ NameContainsFilter.java
@@ -1,7 +1,14 @@
 package filesprocessing;
 
+/**
+ * PrefixFilter class that selects for files with a given prefix.
+ */
 public class NameContainsFilter extends NameFilter {
 
+    /**
+     * NameContainsFilter constructor thar receives a string in the parameters.
+     * @param parameters the string parameter to compare.
+     */
     NameContainsFilter(String[] parameters){
         super(parameters);
     }
