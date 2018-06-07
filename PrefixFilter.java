24f176bc02769d7be8d279edeed8897752c59199
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 23:01:25 +0300

    Update PrefixFilter.java

--- PrefixFilter.java
+++ PrefixFilter.java
@@ -1,7 +1,14 @@
 package filesprocessing;
 
+/**
+ * PrefixFilter class that selects for files with a given prefix.
+ */
 public class PrefixFilter extends NameFilter {
 
+    /**
+     * PrefixFilter constructor thar receives a string in the parameters.
+     * @param parameters the string parameter to compare.
+     */
     PrefixFilter(String[] parameters){
         super(parameters);
     }
