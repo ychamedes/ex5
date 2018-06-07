f43f416926e4323581d3d3c8c0b7b858655bdb4c
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:59:04 +0300

    Update GreaterThanFilter.java

--- GreaterThanFilter.java
+++ GreaterThanFilter.java
@@ -1,7 +1,15 @@
 package filesprocessing;
 
+
+/**
+ * GreaterThanClass that selects for files over a certain size.
+ */
 public class GreaterThanFilter extends SizeFilter {
 
+    /**
+     * GreaterThanFilter Constructor that takes filter's bounds.
+     * @param parameters the filters bounds as a parameters list.
+     */
     GreaterThanFilter(Double[] parameters){
         super(parameters);
     }
