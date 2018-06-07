52c43f8e7076876995de9819fd80fcfeb0af6ebc
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:57:08 +0300

    Update BetweenFilter.java

--- BetweenFilter.java
+++ BetweenFilter.java
@@ -1,7 +1,14 @@
 package filesprocessing;
 
+/**
+ * BetweenThanClass that selects for files with sizes that are between two bounds.
+ */
 public class BetweenFilter extends SizeFilter {
 
+    /**
+     * BetweenThanFilter Constructor that takes filter's bounds.
+     * @param parameters the filters bounds as a parameters list.
+     */
     BetweenFilter(Double[] parameters){
         super(parameters);
     }
