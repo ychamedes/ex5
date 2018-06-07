385e991ddda1208f728e2bc714be08a01b804d51
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:59:15 +0300

    Update HiddenFilter.java

--- HiddenFilter.java
+++ HiddenFilter.java
@@ -2,10 +2,21 @@ package filesprocessing;
 
 import java.io.File;
 
+
+/**
+ * HiddenFilter class that selects for files that are hidden.
+ */
 public class HiddenFilter extends GeneralFilter {
+
+    public HiddenFilter(String attributeParam){
+        super(attributeParam)
+    }
 
     @Override
     protected boolean attribute(File file) {
-        return (file.isHidden());
+        if(generalParam.equals(YES_PARAMETER))
+            return(file.isHidden());
+        else
+            return(!file.isHidden());
     }
 }
