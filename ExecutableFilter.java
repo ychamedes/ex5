eaa27c448afe057c2fd4369c462303aaabed15d1
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:57:44 +0300

    Update ExecutableFilter.java

--- ExecutableFilter.java
+++ ExecutableFilter.java
@@ -2,10 +2,22 @@ package filesprocessing;
 
 import java.io.File;
 
+
+/**
+ * ExecutableFilter class that selects for files that are executable.
+ */
 public class ExecutableFilter extends GeneralFilter {
+
+
+    public ExecutableFilter(String attributeParam){
+        super(attributeParam)
+    }
 
     @Override
     protected boolean attribute(File file) {
-        return(file.canExecute());
+        if(generalParam.equals(YES_PARAMETER))
+            return(file.canExecute());
+        else
+            return(!file.canExecute());
     }
 }
