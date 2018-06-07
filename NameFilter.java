48d3c44cf56df1d5feade3cbf4d3f13ffdfe527f
ychamedes <34750353+ychamedes@users.noreply.github.com>
2018-06-07 22:59:44 +0300

    Update NameFilter.java

--- NameFilter.java
+++ NameFilter.java
@@ -3,19 +3,27 @@ package filesprocessing;
 import java.io.File;
 import java.util.HashSet;
 
+/**
+ * NameFilter class, an abstract filter class that is inherited by filters to compare file names and Strings.
+ */
 public abstract class NameFilter implements Filter{
 
     /** The name to analyze. */
     String namePart;
 
+
+    /**
+     * NameFilter constructor, assigns the parameters to the given String with which we want to compare.
+     * @param parameters list of the String parameters.
+     */
     public NameFilter(String[] parameters){
         namePart = parameters[0];
     }
 
     /**
-     *
-     * @param fileName
-     * @return
+     * Compares the names of two files according to some attribute.
+     * @param fileName the name of the file
+     * @return true if the file name meets the conditions of the bounds.
      */
     protected abstract boolean attribute(String fileName);
 
